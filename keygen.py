d1c05b3028ed5629a91ee80cb7e626f53b82dd67
Aayushi Salbarde <ayushisalbarde09@gmail.com>
2024-10-14 06:35:56 -0400

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -1,16 +1,13 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 
-# Generate RSA private key
 private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
 )
 
-# Extract public key from the private key
 public_key = private_key.public_key()
 
-# Save the private key to a PEM file (optional)
 with open("private_key.pem", "wb") as f:
     f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
@@ -18,7 +15,6 @@ with open("private_key.pem", "wb") as f:
         encryption_algorithm=serialization.NoEncryption()
     ))
 
-# Save the public key to a PEM file
 with open("public_key.pem", "wb") as f:
     f.write(public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
