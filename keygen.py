b8ba649fcf54fb4169867443707798c2ea87a942
Aayushi Salbarde <ayushisalbarde09@gmail.com>
2024-10-14 09:31:51 -0400

    keygen.py

--- keygen.py
+++ keygen.py
@@ -1,22 +1,10 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
+import rsa 
 
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-)
+public_key ,private_key = rsa.newkeys(1024)
 
-public_key = private_key.public_key()
-
-with open("private_key.pem", "wb") as f:
-    f.write(private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    ))
-
-with open("public_key.pem", "wb") as f:
-    f.write(public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    ))
+with open("public.pem","wb") as f :
+    f.write(public_key.save_pkcs1("PEM"))
+    
+with open("private.pem","wb") as f :
+    f.write(private_key.save_pkcs1("PEM"))
+    
