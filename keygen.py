af1014b5ec3dd17b44a6a19d003cc466364d23cb
Aayushi Salbarde <ayushisalbarde09@gmail.com>
2024-10-14 09:38:58 -0400

    keygen.py

--- keygen.py
+++ keygen.py
@@ -2,9 +2,9 @@ import rsa
 
 public_key ,private_key = rsa.newkeys(1024)
 
-with open("public.pem","wb") as f :
+with open("public_key.pem","wb") as f :
     f.write(public_key.save_pkcs1("PEM"))
     
-with open("private.pem","wb") as f :
+with open("private_key.pem","wb") as f :
     f.write(private_key.save_pkcs1("PEM"))
     
