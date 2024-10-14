8ce4ca9a4795325b262ed7aeadaf7a732638dc12
Aayushi Salbarde <ayushisalbarde09@gmail.com>
2024-10-14 06:36:43 -0400

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,24 +1,19 @@
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes, serialization
 
-# Load the public key
 with open("public_key.pem", "rb") as f:
     public_key = serialization.load_pem_public_key(f.read())
 
-# Load the document
 with open("documents.txt", "rb") as f:
     document = f.read()
 
-# Hash the document
 document_hash = hashes.Hash(hashes.SHA256())
 document_hash.update(document)
 digest = document_hash.finalize()
 
-# Load the signature
 with open("signature.sig", "rb") as f:
     signature = f.read()
 
-# Verify the signature
 try:
     public_key.verify(
         signature,
